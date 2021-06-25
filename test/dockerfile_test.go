d0146dc48f94daad42d2a199f0fa13a0116f03c5
Mark Honomichl <mark@honomichl.net>
2021-06-25 13:08:50 -0500

    Fixing test

--- test/dockerfile_test.go
+++ test/dockerfile_test.go
@@ -8,7 +8,7 @@ import (
 
 func TestDockerfile(t *testing.T) {
     tag := "austincloud/terratest"
-    tfVersion := "1.0.0"
+    tfVersion := "1.0.1"
     goVersion := "1.16"
 
     buildOptions := &docker.BuildOptions{
