32ad19265710dc4fb572bb97efa5a93a6a198aa2
Mark Honomichl <mark@honomichl.net>
2021-07-11 10:11:42 -0500

    Upping the version for the test to pass

--- test/dockerfile_test.go
+++ test/dockerfile_test.go
@@ -8,7 +8,7 @@ import (
 
 func TestDockerfile(t *testing.T) {
     tag := "austincloud/terratest"
-    tfVersion := "1.0.1"
+    tfVersion := "1.0.2"
     goVersion := "1.16"
 
     buildOptions := &docker.BuildOptions{
