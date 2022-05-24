2214df810654a8a6b03cddef6bc7274b0f926066
Mark Honomichl <mark@honomichl.net>
2022-05-24 13:50:24 -0500

    Changing to 1.2.1

--- test/dockerfile_test.go
+++ test/dockerfile_test.go
@@ -8,7 +8,7 @@ import (
 
 func TestDockerfile(t *testing.T) {
     tag := "austincloud/terratest"
-    tfVersion := "1.2.0"
+    tfVersion := "1.2.1"
     goVersion := "1.16"
 
     buildOptions := &docker.BuildOptions{
