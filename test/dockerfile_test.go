3e36c38e76c406b50f230115a0a215e4ee2eccfb
Mark Honomichl <mark@honomichl.net>
2022-08-29 09:50:58 -0400

    Updating Version

--- test/dockerfile_test.go
+++ test/dockerfile_test.go
@@ -9,7 +9,7 @@ import (
 
 func TestDockerfile(t *testing.T) {
     tag := "austincloud/terratest"
-    tfVersion := "1.2.7"
+    tfVersion := "1.2.8"
     goVersion := "1.18"
 
     buildOptions := &docker.BuildOptions{
