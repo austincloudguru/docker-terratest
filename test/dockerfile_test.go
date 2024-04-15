9de4ef5536652c88a5896079e6ff6e71e3ed9dc9
Mark Honomichl <mark@honomichl.net>
2024-04-15 09:24:32 -0500

    Updating to v1.5.2

--- test/dockerfile_test.go
+++ test/dockerfile_test.go
@@ -10,7 +10,7 @@ import (
 func TestDockerfile(t *testing.T) {
     tag := "austincloud/terratest"
     // This still needs to be the actual version of terraform
-    tfVersion := "1.5.1"
+    tfVersion := "1.5.2"
     goVersion := "1.22"
 
     buildOptions := &docker.BuildOptions{
